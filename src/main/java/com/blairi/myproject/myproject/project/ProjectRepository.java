707e76dfb2d911d90c4e0bf8aa9e266cb8a18997
Blairi <axel.avi.fer@gmail.com>
2023-04-18 22:14:15 -0600

    project repository

---
+++ src/main/java/com/blairi/myproject/myproject/project/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.blairi.myproject.myproject.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long>{
+
+}
