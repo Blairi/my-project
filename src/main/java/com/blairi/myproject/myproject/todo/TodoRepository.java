a8e659676d86c80ebf144d64b962645b13b16a46
Blairi <axel.avi.fer@gmail.com>
2023-04-19 11:32:06 -0600

    repository annotation added

--- src/main/java/com/blairi/myproject/myproject/todo/TodoRepository.java
+++ src/main/java/com/blairi/myproject/myproject/todo/TodoRepository.java
@@ -1,7 +1,9 @@
 package com.blairi.myproject.myproject.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends JpaRepository<Todo, Long>{
 
 }
--- src/main/java/com/blairi/myproject/myproject/user/UserRepository.java
+++ src/main/java/com/blairi/myproject/myproject/user/UserRepository.java
@@ -1,7 +1,9 @@
 package com.blairi.myproject.myproject.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
