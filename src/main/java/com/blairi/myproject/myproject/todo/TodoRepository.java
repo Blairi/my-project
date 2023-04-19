ce0b0da8a43297c00030493497156d0f584677a2
Blairi <axel.avi.fer@gmail.com>
2023-04-18 22:14:30 -0600

    todo repository

---
+++ src/main/java/com/blairi/myproject/myproject/todo/TodoRepository.java
@@ -0,0 +1,7 @@
+package com.blairi.myproject.myproject.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long>{
+
+}
