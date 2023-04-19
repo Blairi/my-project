f2efea5d1c371003d4b16c9e969c95a492c7bcee
Blairi <axel.avi.fer@gmail.com>
2023-04-18 22:14:56 -0600

    user repository

---
+++ src/main/java/com/blairi/myproject/myproject/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.blairi.myproject.myproject.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
