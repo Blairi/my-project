c3e70d025ccaf901b411effeae40020a8c9989d7
Blairi <axel.avi.fer@gmail.com>
2023-04-19 16:46:45 -0600

    added create a project and view projects functionality

--- src/main/java/com/blairi/myproject/myproject/project/ProjectResource.java
+++ src/main/java/com/blairi/myproject/myproject/project/ProjectResource.java
@@ -1,20 +1,54 @@
 package com.blairi.myproject.myproject.project;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.blairi.myproject.myproject.todo.Todo;
+import com.blairi.myproject.myproject.user.User;
+import com.blairi.myproject.myproject.user.UserService;
 
 @RestController
 public class ProjectResource {
 	
 	@Autowired
 	private ProjectService projectService;
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/projects")
+	public List<Project> retrieveAllProjects() {
+		// TODO: Remove hard coded owner
+		User user = userService.findById(1L);
+		
+		return user.getProjects();
+	}
+	
+	@PostMapping("/projects")
+	public ResponseEntity<Object> createProject(@RequestBody Project project) {
+		
+		// TODO: Remove hard coded owner
+		User user = userService.findById(1L);
+		project.setUser(user);
+		
+		Project savedProject = projectService.save(project);
+		
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedProject.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+	}
 	
 	@GetMapping("/projects/{id}")
 	public Project retrieveProjectById(@PathVariable Long id) {
