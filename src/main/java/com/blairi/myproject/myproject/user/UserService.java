9da86375eaea507528e275efce45e4fec634ee6b
Blairi <axel.avi.fer@gmail.com>
2023-04-19 11:52:48 -0600

    user service

---
+++ src/main/java/com/blairi/myproject/myproject/user/UserService.java
@@ -0,0 +1,23 @@
+package com.blairi.myproject.myproject.user;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User findById(Long id) {
+		
+		Optional<User> userFound = userRepository.findById(id);
+		
+		if(userFound.isEmpty()) throw new RuntimeException("User not found");
+		
+		return userFound.get();
+	}
+	
+}
