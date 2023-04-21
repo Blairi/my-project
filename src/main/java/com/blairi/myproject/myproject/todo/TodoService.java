c63111f40a1f3501f7459d71ec6fd098a148727e
Blairi <axel.avi.fer@gmail.com>
2023-04-20 18:55:56 -0600

    added find todo by id method

--- src/main/java/com/blairi/myproject/myproject/todo/TodoService.java
+++ src/main/java/com/blairi/myproject/myproject/todo/TodoService.java
@@ -1,5 +1,8 @@
 package com.blairi.myproject.myproject.todo;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,15 @@ public class TodoService {
 	
 	public Todo save(Todo todo) {
 		return todoRepository.save(todo);
+	}
+	
+	public Todo findById(Long id) {
+		
+		Optional<Todo> todoFound = todoRepository.findById(id);
+		
+		if(todoFound.isEmpty()) throw new RuntimeException("Todo not found");
+		
+		return todoFound.get();
 	}
 	
 }
