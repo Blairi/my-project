cce6a3637fdcf56544919aad7c73efc8131fefa2
Blairi <axel.avi.fer@gmail.com>
2023-04-27 21:39:02 -0600

    user resources added

---
+++ src/main/java/com/blairi/myproject/myproject/user/UserResource.java
@@ -0,0 +1,19 @@
+package com.blairi.myproject.myproject.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/user/{id}")
+	public User retrieveUser(@PathVariable Long id) {
+		return userService.findById(id);
+	}
+	
+}
